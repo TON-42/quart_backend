d6a06a5385bc6ff1f849628b02edccb57cf3a810
dantol29 <dantol29@gmail.com>
2024-06-29 22:57:39 +0300

    fix: add chat_users to utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
 from shared import user_clients
 
 async def get_chat_id(dialog_id, sender_id, phone_number):
+    chat_users = []
     users = await user_clients[phone_number].get_client().get_participants(dialog_id)
     for user in users:
         chat_users.append(user.id)
     chat_users.append(sender_id)
     private_chat_id = '_'.join(str(num) for num in sorted(chat_users))
     return (private_chat_id)
-    
