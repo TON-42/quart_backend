e3b4a18acdeb26ff1c86b902fba04ab64a4edfe5
lmangall <l.mangallon@gmail.com>
2024-07-04 17:13:44 +0200

    fix(/add-user-to-agreed): put function back

--- src/routes/chat_route.py
+++ src/routes/chat_route.py
@@ -120,3 +120,70 @@ async def send_message():
         return jsonify({"error": "couldn't update auth_status"}), 500
 
     return jsonify({"userB": b_users if b_users else None}), 200
+
+
+@chat_route.route("/add-user-to-agreed", methods=["POST"])
+async def add_user_to_agreed():
+    session = Session()
+    try:
+        data = await request.get_json()
+        print(f"add-user-to-agreed: {data}")
+        if not isinstance(data, list):
+            return jsonify({"error": "Input data should be an array"}), 400
+        chat_status = {}
+        for entry in data:
+            user_id = entry.get("userId")
+            chat_id = entry.get("chatId")
+            
+            if user_id is None or chat_id is None:
+                chat_status[chat_id] = "error"
+                continue
+
+            chat_status[chat_id] = "error"
+
+            # get user
+            try:
+                user = session.query(User).options(
+                    joinedload(User.chats)).filter(User.id == user_id).one()
+            except Exception as e:
+                print(f"Error: {str(e)}")
+                continue
+
+            # get chat
+            try:
+                chat = session.query(Chat).options(
+                    joinedload(Chat.agreed_users),
+                    joinedload(Chat.users)
+                ).filter(Chat.id == chat_id).one()
+            except Exception as e:
+                print(f"Error: {str(e)}")
+                continue
+
+            # if chat is already sold
+            if chat.status == ChatStatus.sold:
+                chat_status[chat_id] = "sold"
+                continue
+
+            for chat_user in chat.users:
+                # if user exists in the chat 
+                if chat_user.id == user_id:
+                    for user_agreed in chat.agreed_users:
+                        # if user has already agreed
+                        if user_agreed.id == user_id:
+                            break
+                    chat.agreed_users.append(user)
+                    chat_status[chat_id] = "pending"
+                    break
+
+            # if all users have agreed
+            if len(chat.agreed_users) == len(chat.users):
+                chat.status = ChatStatus.sold
+                chat_status[chat_id] = "sold"
+                await chat_sale(chat.users)
+            session.commit()
+
+        session.close()
+        return jsonify(chat_status), 200
+    except Exception as e:
+        session.close()
+        return jsonify({"error": str(e)}), 500
