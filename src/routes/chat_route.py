76545840738b3640464590b358e38c89dff0425b
lmangall <l.mangallon@gmail.com>
2024-07-04 16:03:50 +0200

    fix(entity problem)handle input entity retrieval for PeerUser and PeerChat in send_message

--- src/routes/chat_route.py
+++ src/routes/chat_route.py
@@ -32,9 +32,9 @@ async def send_message():
         message = "Hello! The owner of this chat wants to sell the data of this chat.\nPlease click the button below to accept the sale and proceed to the bot:"
     else:
         message_for_second_user = (
-            message + "\n\n"
-            "https://t.me/chatpayapp_bot/chatpayapp"
-        )
+        message + "\n\n"
+        "https://t.me/chatpayapp_bot/chatpayapp"
+    )
 
     selected_chats = data.get("chats", {})
     if not selected_chats:
@@ -52,15 +52,12 @@ async def send_message():
     if await connect_client(client, phone_number, user_id) == -1:
         return jsonify({"error": "error in connecting to Telegram"}), 500
     
-    if await client.is_user_authorized() == False:
+    if not await client.is_user_authorized():
         print("Session is expired or user manually logged out")
         return jsonify("Session is expired or user manually logged out"), 500
     
-    sender = None
-
-    if await client.is_user_authorized() == True:
-        sender = await client.get_me()
-    else:
+    sender = await client.get_me()
+    if not sender:
         print("User manually logged out")
         return jsonify("User manually logged out"), 500
 
@@ -72,14 +69,8 @@ async def send_message():
             # TODO: make data parsing as separate func or remove tuples
             # Extract chat_id and chat_name from 'id' field
             id_field = str(chat_details)
-            
-            # Remove the surrounding parentheses
             id_field_clean = id_field.strip("()")
-            
-            # Split the cleaned id_field by ", '"
             chat_id_str, chat_name_str = id_field_clean.split(", '", 1)
-            
-            # Convert chat_id_str to an integer and clean chat_name_str
             chat_id = int(chat_id_str)
             chat_name = chat_name_str[:-1]  # Remove the trailing single quote
             
@@ -96,7 +87,10 @@ async def send_message():
                 print("words is not defined")
                 words = 123
 
-            users = await client.get_participants(chat_id)
+            # Fetch chat entity before getting participants
+            chat_entity = await client.get_entity(PeerChat(chat_id))
+            users = await client.get_participants(chat_entity)
+            
             for user in users:
                 print(f"Creating {user.username} account")
                 await create_user(user.id, user.username, False)
@@ -118,7 +112,7 @@ async def send_message():
             chat_users.clear()
         except Exception as e:
             print(f"Error in send_message(): {str(e)}")
-            if await client.is_user_authorized() == True:
+            if await client.is_user_authorized():
                 await client.log_out()
             await delete_session(phone_number, user_id)
             return {"error": str(e)}, 500
@@ -128,69 +122,3 @@ async def send_message():
         return jsonify({"error": "couldn't update auth_status"}), 500
 
     return jsonify({"userB": b_users if b_users else None}), 200
-
-@chat_route.route("/add-user-to-agreed", methods=["POST"])
-async def add_user_to_agreed():
-    session = Session()
-    try:
-        data = await request.get_json()
-        print(f"add-user-to-agreed: {data}")
-        if not isinstance(data, list):
-            return jsonify({"error": "Input data should be an array"}), 400
-        chat_status = {}
-        for entry in data:
-            user_id = entry.get("userId")
-            chat_id = entry.get("chatId")
-            
-            if user_id is None or chat_id is None:
-                chat_status[chat_id] = "error"
-                continue
-
-            chat_status[chat_id] = "error"
-
-            # get user
-            try:
-                user = session.query(User).options(
-                    joinedload(User.chats)).filter(User.id == user_id).one()
-            except Exception as e:
-                print(f"Error: {str(e)}")
-                continue
-
-            # get chat
-            try:
-                chat = session.query(Chat).options(
-                    joinedload(Chat.agreed_users),
-                    joinedload(Chat.users)
-                ).filter(Chat.id == chat_id).one()
-            except Exception as e:
-                print(f"Error: {str(e)}")
-                continue
-
-            # if chat is already sold
-            if chat.status == ChatStatus.sold:
-                chat_status[chat_id] = "sold"
-                continue
-
-            for chat_user in chat.users:
-                # if user exists in the chat 
-                if chat_user.id == user_id:
-                    for user_agreed in chat.agreed_users:
-                        # if user has already agreed
-                        if user_agreed.id == user_id:
-                            break
-                    chat.agreed_users.append(user)
-                    chat_status[chat_id] = "pending"
-                    break
-
-            # if all users have agreed
-            if len(chat.agreed_users) == len(chat.users):
-                chat.status = ChatStatus.sold
-                chat_status[chat_id] = "sold"
-                await chat_sale(chat.users)
-            session.commit()
-
-        session.close()
-        return jsonify(chat_status), 200
-    except Exception as e:
-        session.close()
-        return jsonify({"error": str(e)}), 500
