b62662f1d4ec6ce4b18a4d72fdea1d74c62cabf3
552020 <stefanolombardo@posteo.de>
2024-07-13 11:35:00 +0200

    refactor: rename S into DBSession and session into db_session

--- src/services/session_service.py
+++ src/services/session_service.py
@@ -1,20 +1,26 @@
-from db import Session as S
+from db import Session as DBSession
 from models import User, Chat, Session, ChatStatus
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm import joinedload
 from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
 
+
 async def create_session(client, number, phone_hash, userId):
     session_id = client.session.save()
-    session = S()
+    db_session = DBSession()
     exit_code = 0
     try:
         if userId is None:
             userId = "None"
-        new_session = Session(id=session_id, phone_number=number, phone_code_hash=phone_hash, user_id=userId)
-        session.add(new_session)
-        session.commit()
+        new_session = Session(
+            id=session_id,
+            phone_number=number,
+            phone_code_hash=phone_hash,
+            user_id=userId,
+        )
+        db_session.add(new_session)
+        db_session.commit()
         print(f"Creating new session for {number}({userId})")
     except Exception as e:
         print(f"Error creating new session: {str(e)}")
@@ -23,83 +29,99 @@ async def create_session(client, number, phone_hash, userId):
         session.close()
         return exit_code
 
+
 async def session_exists(number, userId):
     print(f"session_exists: {number}, user_id: {userId}")
-    session = S()
+    db_session = DBSession()
     exit_code = 0
     try:
         if number is not None:
             print("Trying to find a session with phone number")
-            found_session = session.query(Session).filter(Session.phone_number == number).one()
+            found_session = (
+                db_session.query(Session).filter(Session.phone_number == number).one()
+            )
         else:
             print("Trying to find a session with user_id")
-            found_session = session.query(Session).filter(Session.user_id == str(userId)).one()
-        session.close()
+            found_session = (
+                db_session.query(Session).filter(Session.user_id == str(userId)).one()
+            )
+        db_session.close()
         return found_session
     except NoResultFound:
         print("session is not found :(")
-        session.close()
+        db_session.close()
         return None
     except Exception as e:
         print(f"Error while looking for a session: {str(e)}")
-        session.close()
+        db_session.close()
         return None
 
+
 async def delete_session(number, userId):
-    session = S()
+    db_session = DBSession()
     exit_code = 0
     try:
         if number is None:
-            found_session = session.query(Session).filter(Session.user_id == userId).all()
+            found_session = (
+                db_session.query(Session).filter(Session.user_id == userId).all()
+            )
         else:
-            found_session = session.query(Session).filter(Session.phone_number == number).all()
+            found_session = (
+                db_session.query(Session).filter(Session.phone_number == number).all()
+            )
 
         for one_session in found_session:
-            session.delete(one_session)
-    
-        session.commit()
-        session.close()
+            db_session.delete(one_session)
+
+        db_session.commit()
+        db_session.close()
         return True
     except NoResultFound:
         print("Session in delete_session is not found")
-        session.close()
+        db_session.close()
         return False
     except Exception as e:
         print(f"Error while deleting for a session: {str(e)}")
-        session.close()
+        db_session.close()
         return False
 
+
 async def set_session_is_logged_and_user_id(number, sender_id):
-    session = S()
+    db_session = DBSession()
     exit_code = 0
     try:
-        found_session = session.query(Session).filter(Session.phone_number == number).one()
+        found_session = (
+            db_session.query(Session).filter(Session.phone_number == number).one()
+        )
         found_session.is_logged = True
         found_session.user_id = sender_id
-        session.commit()
-        session.close()
+        db_session.commit()
+        db_session.close()
         return True
     except NoResultFound:
-        session.close()
+        db_session.close()
         return False
     except Exception as e:
         print(f"Error in set_session_is_logged_and_user_id(): {str(e)}")
-        session.close()
+        db_session.close()
         return False
 
+
 async def set_session_chats(number, all_chats):
-    session = S()
+    db_session = DBSession()
     exit_code = 0
     try:
-        found_session = session.query(Session).filter(Session.phone_number == number).one()
+        found_session = (
+            db_session.query(Session).filter(Session.phone_number == number).one()
+        )
         found_session.chats = all_chats
-        session.commit()
-        session.close()
+        db_session.commit()
+        db_session.close()
         return True
     except NoResultFound:
-        session.close()
+        db_session.close()
         return False
     except Exception as e:
         print(f"Error in set_session_chats(): {str(e)}")
-        session.close()
+        db_session.close()
         return False
