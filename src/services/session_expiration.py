78fce838d2901b70d45dee9a9013e471e3ea6382
552020 <stefanolombardo@posteo.de>
2024-07-22 09:15:12 +0200

    fix: substitute get_sqlalchem_session with its persisten sibling

--- src/services/session_expiration.py
+++ src/services/session_expiration.py
@@ -2,7 +2,7 @@
 
 from datetime import datetime, timedelta
 from config import Config
-from db import get_sqlalchemy_session
+from db import get_persistent_sqlalchemy_session
 from models import Session as SessionModel
 from services.session_service import delete_session
 from telethon.sessions import StringSession
@@ -15,37 +15,35 @@ logger = logging.getLogger(__name__)
 
 async def check_session_expiration():
     while True:
-        async with get_sqlalchemy_session() as db_session:
-            try:
-                all_sessions = db_session.query(SessionModel).all()
+        db_session = get_persistent_sqlalchemy_session()
+        try:
+            all_sessions = db_session.query(SessionModel).all()
 
-                for user_session in all_sessions:
-                    time_difference = datetime.now() - user_session.creation_date
-                    if time_difference >= timedelta(
-                        minutes=Config.SESSION_EXPIRATION_MINUTES
-                    ):
-                        logger.info(
-                            f"Session for {user_session.phone_number} has expired."
-                        )
-                        if user_session.is_logged:
-                            try:
-                                client = TelegramClient(
-                                    StringSession(user_session.id),
-                                    Config.API_ID,
-                                    Config.API_HASH,
-                                )
-                                await client.connect()
-                                if await client.is_user_authorized():
-                                    await client.log_out()
-                            except Exception as e:
-                                logger.error(f"Error in log_out(): {str(e)}")
-                            finally:
-                                await client.disconnect()
-                        await delete_session(user_session.phone_number, None)
-                    else:
-                        logger.info(
-                            f"Session for: {user_session.phone_number} is active"
-                        )
-            except Exception as e:
-                logger.error(f"Database error: {str(e)}")
+            for user_session in all_sessions:
+                time_difference = datetime.now() - user_session.creation_date
+                if time_difference >= timedelta(
+                    minutes=Config.SESSION_EXPIRATION_MINUTES
+                ):
+                    logger.info(f"Session for {user_session.phone_number} has expired.")
+                    if user_session.is_logged:
+                        try:
+                            client = TelegramClient(
+                                StringSession(user_session.id),
+                                Config.API_ID,
+                                Config.API_HASH,
+                            )
+                            await client.connect()
+                            if await client.is_user_authorized():
+                                await client.log_out()
+                        except Exception as e:
+                            logger.error(f"Error in log_out(): {str(e)}")
+                        finally:
+                            await client.disconnect()
+                    await delete_session(user_session.phone_number, None)
+                else:
+                    logger.info(f"Session for: {user_session.phone_number} is active")
+        except Exception as e:
+            logger.error(f"Database error: {str(e)}")
+        finally:
+            db_session.close()
         await asyncio.sleep(Config.CHECK_INTERVAL_SECONDS)
